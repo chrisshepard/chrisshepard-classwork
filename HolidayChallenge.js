6bdf4b2e63f2b7af9daf5233e9ed0599669649e6
MCC Local Administrator <cshepard211@gmail.com>
2018-10-04 18:28:00 -0400

    Quiz1

---
+++ HolidayChallenge.js
@@ -0,0 +1,9 @@
+var kmRowed = 200000;
+
+var firstHalf = .02;
+var secondHalf = .04;
+
+
+var charityTotal = (100000 * firstHalf) + (100000 * secondHalf)
+
+console.log("I will raise $" + charityTotal + " for charity this year!")
